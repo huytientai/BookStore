514ce0a3d5334708a54445a097748df724746ebb
huy nguyen <huytientai@gmail.com>
2019-10-26 20:46:37 +0700

    new project

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<html>
+@include('layouts.head')
+<body>
+@include('layouts.header')
+
+<div class="container">
+    @yield('content')
+</div>
+
+{{--@include('layouts.footer')--}}
+</body>
+</html>
