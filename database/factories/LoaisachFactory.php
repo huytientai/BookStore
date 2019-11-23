87e257b32ef992ea6c234674c32c5a6366c09a41
Huong-hihi <55866398+Huong-hihi@users.noreply.github.com>
2019-11-23 18:28:18 +0700

    Update LoaisachFactory.php

--- database/factories/LoaisachFactory.php
+++ database/factories/LoaisachFactory.php
@@ -7,6 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(loaisach::class, function (Faker $faker) {
     return [
-        'tenloaisach' => $faker->name,
+        'nametype' => $faker->name,
     ];
 });
