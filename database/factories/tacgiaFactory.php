9c6ddf34255d420cb682f2c30a12238b57e0772f
linhntt011199 <57032418+linhntt011199@users.noreply.github.com>
2019-11-23 11:25:19 +0700

    Update tacgiaFactory.php

--- database/factories/tacgiaFactory.php
+++ database/factories/tacgiaFactory.php
@@ -7,9 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Tacgia::class, function (Faker $faker) {
     return [
-		'tacgias_name' => $faker->unique()->name,
-		'tacgias_email' => $faker->unique()->safeEmail,
-		'tacgias_sdt' => $faker->phoneNumber,
-		'tacgias_diachi' => $faker->address
+		'name' => $faker->unique()->name,
+		'email' => $faker->unique()->safeEmail,
+		'sdt' => $faker->phoneNumber,
+		'diachi' => $faker->address
     ];
 });
