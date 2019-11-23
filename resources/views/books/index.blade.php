c951746405a4deb9a447df6e47561a2470dd9053
huy nguyen <huytientai@gmail.com>
2019-11-23 17:29:33 +0700

    comment

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,22 +5,35 @@
 @section('content')
     @include('flash::message')
 
+{{--    <h1>Books</h1>--}}
+{{--    @if(count($books)>0)--}}
+{{--        @foreach($books as $key => $book)--}}
+{{--            <div class="card card-body bg-light">--}}
+{{--                <div class="row">--}}
+{{--                    --}}{{--                card card-body bg-light--}}
+{{--                    <div class="col-xs-4">xxxxxxxxxxxx</div>--}}
+{{--                    <div class="col-xs-8">--}}
+{{--                        <div>--}}
+{{--                            <h3>{{ $books->firstItem() + $key }} . <a href="/books/{{$book->id}}"> {{ $book->name }}</a>--}}
+{{--                            </h3>--}}
+{{--                            <p> DESC: {!! $book->desc !!}</p>--}}
+{{--                            <small>update on {{ $book->created_at }}</small>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <br>--}}
+{{--        @endforeach--}}
+{{--    @endif--}}
+{{--    {!! $books->links() !!}--}}
+
     <h1>Books</h1>
     @if(count($books)>0)
         @foreach($books as $key => $book)
             <div class="card card-body bg-light">
-                <div class="row">
-                    {{--                card card-body bg-light--}}
-                    <div class="col-xs-4">xxxxxxxxxxxx</div>
-                    <div class="col-xs-8">
-                        <div>
-                            <h3>{{ $books->firstItem() + $key }} . <a href="/books/{{$book->id}}"> {{ $book->name }}</a>
-                            </h3>
-                            <p> DESC: {!! $book->desc !!}</p>
-                            <small>update on {{ $book->created_at }}</small>
-                        </div>
-                    </div>
-                </div>
+                <h3>{{ $books->firstItem() + $key }} . <a href="/books/{{$book->id}}"> {{ $book->name }}</a></h3>
+                <p> DESC: {!! $book->desc !!}</p>
+                <small>update on {{ $book->created_at }}</small>
             </div>
             <br>
         @endforeach
