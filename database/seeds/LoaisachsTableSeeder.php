39e473d9b1ee896404bfc25d772cdb6b5157cedc
Huong-hihi <55866398+Huong-hihi@users.noreply.github.com>
2019-11-23 18:30:41 +0700

    Update LoaisachsTableSeeder.php

--- database/seeds/LoaisachsTableSeeder.php
+++ database/seeds/LoaisachsTableSeeder.php
@@ -18,16 +18,16 @@ class loaisachSeeder extends Seeder
         factory(\App\Models\loaisach::class, 10)->create();
 
         \App\Models\loaisach::create([
-            ['id' => '1','tenloaisach' => 'truyện cười','created_at'=>'2019-02-02','updated_at'=>'2019-03-02' ],
-            ['id' => '2','tenloaisach' => 'truyện trinh thám','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
-            ['id' => '3','tenloaisach' => 'truyện cổ tích','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
-            ['id' => '4','tenloaisach' => 'truyện ngụ ngôn','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
-            ['id' => '5','tenloaisach' => 'sách tâm lý','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
-            ['id' => '6','tenloaisach' => 'sách giáo dục','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
-            ['id' => '7','tenloaisach' => 'sách kinh doanh','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
-            ['id' => '8','tenloaisach' => 'sách cho người già','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
-            ['id' => '9','tenloaisach' => 'sách cho mẹ và bé','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
-            ['id' => '10','tenloaisach' => 'sách hạt giống tâm hồn','created_at'=>'2019-02-02','updated_at'=>'2019-03-02']
+            ['typesofbook_id' => '1','nametype' => 'truyện cười','created_at'=>'2019-02-02','updated_at'=>'2019-03-02' ],
+            ['typesofbook_id' => '2','nametype' => 'truyện trinh thám','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
+            ['typesofbook_id' => '3','nametype' => 'truyện cổ tích','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
+            ['typesofbook_id' => '4','nametype' => 'truyện ngụ ngôn','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
+            ['typesofbook_id' => '5','nametype' => 'sách tâm lý','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
+            ['typesofbook_id' => '6','nametype' => 'sách giáo dục','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
+            ['typesofbook_id' => '7','nametype' => 'sách kinh doanh','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
+            ['typesofbook_id' => '8','nametype' => 'sách cho người già','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
+            ['typesofbook_id' => '9','nametype' => 'sách cho mẹ và bé','created_at'=>'2019-02-02','updated_at'=>'2019-03-02'],
+            ['typesofbook_id' => '10','nametype' => 'sách hạt giống tâm hồn','created_at'=>'2019-02-02','updated_at'=>'2019-03-02']
 
             //            'role' => \App\Models\User::ADMIN,
         ]);
