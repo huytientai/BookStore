34cf944868ca6d5f2aca8b8dcf52482fa0831811
quanglinh2545 <57183661+quanglinh2545@users.noreply.github.com>
2019-11-23 14:21:21 +0700

    Update nxbFactory.php

--- database/factories/nxbFactory.php
+++ database/factories/nxbFactory.php
@@ -4,10 +4,10 @@ use App\Models\Nhaxuatban;
 use Faker\Generator as Faker;
 $factory->define(Nhaxuatban::class, function (Faker $faker) {
     return [
-		'nxb_ten' => $faker->unique()->name,
-		'nxb_diachi' => $faker->address,
-		'nxb_sdt' => $faker->unique()->phoneNumber,
-		'nxb_email' => $faker->unique()->safeEmail,
-                'nxb_website' => $faker->unique()->name  
+		'ten' => $faker->unique()->name,
+		'diachi' => $faker->address,
+		'sdt' => $faker->unique()->phoneNumber,
+		'email' => $faker->unique()->safeEmail,
+                'website' => $faker->unique()->name  
 ];
 });
