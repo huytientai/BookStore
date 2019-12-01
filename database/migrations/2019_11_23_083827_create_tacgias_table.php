62d2c82e3303cde28c897f7fcc675de2efdc0421
linhntt011199 <nguyenthithuylinh01111999@gmail.com>
2019-12-01 23:58:17 +0700

    lotteler

--- database/migrations/2019_11_23_083827_create_tacgias_table.php
+++ database/migrations/2019_11_23_083827_create_tacgias_table.php
@@ -14,7 +14,7 @@ class CreateTacgiasTable extends Migration
     public function up()
     {
         Schema::create('tacgias', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
 			$table->string('name');
 			$table->string('email');
 			$table->string('phone');
