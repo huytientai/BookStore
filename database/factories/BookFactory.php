97b4e810d6a4e494e3ffbda2edd38a049eda5ff8
linhntt011199 <nguyenthithuylinh01111999@gmail.com>
2019-12-10 23:26:50 +0700

    a

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -11,5 +11,6 @@ $factory->define(Book::class, function (Faker $faker) {
         'desc' => $faker->paragraph,
         'loaisach_id' => $faker->randomElement([11,12,13,14]),
         'price' => $faker->randomFloat(0.5,10,100),
+		'tacgia_id' => $faker->randomElement([1,2,3,4,5,6,7,8,9,10]),
     ];
 });
