186878ab35937cbdc5e5b322868df678257b5a42
huy nguyen <huytientai1@gmail.com>
2019-12-14 12:47:33 +0700

    asd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Orderdetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -27,11 +29,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $b0 = $this->book->orderBy('name')->limit(10)->get();
-        $b1 = $this->book->findLoaisach(11)->limit(10)->get();
-        $b2 = $this->book->findLoaisach(12)->limit(10)->get();
-        $b3 = $this->book->findLoaisach(13)->limit(10)->get();
-        $b4 = $this->book->findLoaisach(14)->limit(10)->get();
+        $b0 = Orderdetail::groupBy('book_id')->orderByRaw('sum(quantity) desc')->limit(10)->get()->pluck('book');
+
+        $id = $this->book->findLoaisach(11)->pluck('id');
+        $b1 = Orderdetail::whereIn('book_id', $id)->groupBy('book_id')->orderByRaw('sum(quantity) desc')->limit(10)->get()->pluck('book');
+
+        $id = $this->book->findLoaisach(12)->pluck('id');
+        $b2 = Orderdetail::whereIn('book_id', $id)->groupBy('book_id')->orderByRaw('sum(quantity) desc')->limit(10)->get()->pluck('book');
+
+        $id = $this->book->findLoaisach(13)->pluck('id');
+        $b3 = Orderdetail::whereIn('book_id', $id)->groupBy('book_id')->orderByRaw('sum(quantity) desc')->limit(10)->get()->pluck('book');
+
+        $id = $this->book->findLoaisach(14)->pluck('id');
+        $b4 = Orderdetail::whereIn('book_id', $id)->groupBy('book_id')->orderByRaw('sum(quantity) desc')->limit(10)->get()->pluck('book');
 
         return view('home')->with(['b0' => $b0, 'b1' => $b1, 'b2' => $b2, 'b3' => $b3, 'b4' => $b4]);
     }
