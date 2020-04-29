b3233c6c0a9959fb85ddafd0dcfad6b4aaa83331
huy nguyen <huytientai1@gmail.com>
2020-04-29 22:31:51 +0700

    basic chart

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -15,7 +15,7 @@ class ChartController extends Controller
     protected $orderDetail;
     protected $category;
 
-    public function __construct( Order $order, Orderdetail $orderDetail,Loaisach $category)
+    public function __construct(Order $order, Orderdetail $orderDetail, Loaisach $category)
     {
         $this->order = $order;
         $this->orderDetail = $orderDetail;
@@ -27,49 +27,115 @@ class ChartController extends Controller
      */
     public function index()
     {
-        $lineChart = new Revenue();
-        $lineChart->labels(range(1, 31, 1));
+        //---------------------        Last Month           ------------------
 
-        $total = $this->order->select(DB::raw("DATE_FORMAT(created_at,'%d') as date,sum(total_price)"))->where('created_at', '>=', now()->subDays(30)->toDateTimeString())->groupBy('date')->orderBy('date')->get()->pluck('sum(total_price)', 'date')->toArraY();
-        for ($i = 1; $i < 32; $i++) {
-            if (!isset($total[$i])) {
-                $total[$i] = 0;
+        // line chart
+        $lineLastMonthChart = $this->getTotalRevenueEachDay('last month');
+
+        $categories = $this->category->all()->pluck('name', 'id')->toArray();
+        foreach ($categories as $key => $value) {
+            $total = $this->getDayRevenueByCategory($key, 'last month');
+            $lineLastMonthChart->dataset($value, 'line', array_values($total))->color('#' . substr(str_shuffle('ABCDEF0123456789'), 0, 6));
+        }
+
+        // doughnut chart
+        $categories = $this->getMonthRevenueEachCategory('last month');
+        $doughnutLastMonthChart = new Revenue();
+        if (count($categories) == 0) {
+            $doughnutLastMonthChart->labels(['Total']);
+            $doughnutLastMonthChart->dataset('sum', 'doughnut', [0])->backgroundColor('white');
+        } else {
+            $doughnutLastMonthChart->labels($categories->pluck('name')->toArray());
+
+            for ($i = 0; $i < count($categories); $i++) {
+                $colors[$i] = '#' . substr(str_shuffle('ABCDEF0123456789'), 0, 6);
             }
-        }
-        ksort($total);
-        $lineChart->dataset('Total', 'line', array_values($total))->color('red');
-
-        $categories = $this->category->all()->pluck('id','name')->toArray();
-        foreach ($categories as $key => $value ) {
-            $total=$this->getDayRevenueByCategory($value);
-            $lineChart->dataset($key, 'line',array_values($total))->color('#' . substr(str_shuffle('ABCDEF0123456789'), 0, 6));
+            $doughnutLastMonthChart->dataset('sum', 'doughnut', $categories->pluck('revenue')->toArray())->backgroundColor($colors);
         }
 
-        $barChart = new Revenue();
+        //-------------------      Current Month           -------------------
 
-        $barChart->labels(range(1, 31, 1));
+        // line chart
+        $lineCurrentMonthChart = $this->getTotalRevenueEachDay();
 
-        $total = $this->order->select(DB::raw("DATE_FORMAT(created_at,'%d') as date,sum(total_price)"))->where('created_at', '>=', now()->subDays(30)->toDateTimeString())->groupBy('date')->orderBy('date')->get()->pluck('sum(total_price)', 'date')->toArraY();
-        for ($i = 1; $i < 32; $i++) {
-            if (!isset($total[$i])) {
-                $total[$i] = 0;
+        $categories = $this->category->all()->pluck('name', 'id')->toArray();
+        foreach ($categories as $key => $value) {
+            $total = $this->getDayRevenueByCategory($key);
+            $lineCurrentMonthChart->dataset($value, 'line', array_values($total))->color('#' . substr(str_shuffle('ABCDEF0123456789'), 0, 6));
+        }
+
+        // doughnut chart
+        $categories = $this->getMonthRevenueEachCategory();
+        $doughnutCurrentMonthChart = new Revenue();
+        if (count($categories) == 0) {
+            $doughnutCurrentMonthChart->labels(['Total']);
+            $doughnutCurrentMonthChart->dataset('sum', 'doughnut', [0])->backgroundColor('white');
+        } else {
+            $doughnutCurrentMonthChart->labels($categories->pluck('name')->toArray());
+
+            for ($i = 0; $i < count($categories); $i++) {
+                $colors[$i] = '#' . substr(str_shuffle('ABCDEF0123456789'), 0, 6);
             }
-        }
-        ksort($total);
-
-        $barChart->dataset('Total', 'line', array_values($total))->color('red');
-        $categories = $this->category->all()->pluck('id','name')->toArray();
-        foreach ($categories as $key => $value ) {
-            $total=$this->getDayRevenueByCategory($value);
-            $barChart->dataset($key, 'line',array_values($total))->color('#' . substr(str_shuffle('ABCDEF0123456789'), 0, 6));
+            $doughnutCurrentMonthChart->dataset('sum', 'doughnut', $categories->pluck('revenue')->toArray())->backgroundColor($colors);
         }
 
-        return view('charts.index', compact('lineChart','barChart'));
+        return view('charts.index', compact('lineLastMonthChart', 'doughnutLastMonthChart', 'lineCurrentMonthChart', 'doughnutCurrentMonthChart'));
     }
 
-    public function getDayRevenueByCategory($id)
+
+    //--------------------         Support Functions     ------------------------
+
+    public function getTotalRevenueEachDay($time = null)
     {
-        $days = DB::table('orders')->leftJoin('orderdetails', 'orders.id', '=', 'orderdetails.order_id')->leftJoin('books', 'orderdetails.book_id', '=', 'books.id')->select(DB::raw("DATE_FORMAT(orders.created_at,'%d') as date,sum(sell_price*quantity) as sum"))->where('loaisach_id', '=', $id)->where('orders.created_at', '>=', now()->subDays(30)->toDateTimeString())->groupBy('date')->get()->pluck('sum', 'date')->toArray();
+        $chart = new Revenue();
+        $chart->labels(range(1, 31, 1));
+        $date = now();
+
+        if ($time == 'last month') {
+            $first_date = strtotime(date("Y-m-d", strtotime($date)) . ", first day of last month");
+            $first_date = date("Y-m-d", $first_date);
+
+            $last_date = strtotime(date("Y-m-d", strtotime($date)) . ", last day of last month");
+            $last_date = date("Y-m-d", $last_date);
+        } else {
+            $first_date = strtotime(date("Y-m-d", strtotime($date)) . ", first day of this month");
+            $first_date = date("Y-m-d", $first_date);
+
+            $last_date = strtotime(date("Y-m-d", strtotime($date)) . ", last day of this month");
+            $last_date = date("Y-m-d", $last_date);
+        }
+
+        $total = $this->order->select(DB::raw("DATE_FORMAT(created_at,'%d') as date,sum(total_price)"))->where('created_at', '>=', $first_date)->where('created_at', '<=', $last_date)->groupBy('date')->orderBy('date')->get()->pluck('sum(total_price)', 'date')->toArraY();
+        for ($i = 1; $i < 32; $i++) {
+            if (!isset($total[$i])) {
+                $total[$i] = 0;
+            }
+        }
+        ksort($total);
+
+        $chart->dataset('Total', 'line', array_values($total))->color('red');
+        return $chart;
+    }
+
+    public function getDayRevenueByCategory($id, $time = null)
+    {
+        $date = now();
+
+        if ($time == 'last month') {
+            $first_date = strtotime(date("Y-m-d", strtotime($date)) . ", first day of last month");
+            $first_date = date("Y-m-d", $first_date);
+
+            $last_date = strtotime(date("Y-m-d", strtotime($date)) . ", last day of last month");
+            $last_date = date("Y-m-d", $last_date);
+        } else {
+            $first_date = strtotime(date("Y-m-d", strtotime($date)) . ", first day of this month");
+            $first_date = date("Y-m-d", $first_date);
+
+            $last_date = strtotime(date("Y-m-d", strtotime($date)) . ", last day of this month");
+            $last_date = date("Y-m-d", $last_date);
+        }
+
+        $days = DB::table('orders')->leftJoin('orderdetails', 'orders.id', '=', 'orderdetails.order_id')->leftJoin('books', 'orderdetails.book_id', '=', 'books.id')->select(DB::raw("DATE_FORMAT(orders.created_at,'%d') as date,sum(sell_price*quantity) as sum"))->where('loaisach_id', '=', $id)->where('orders.created_at', '>=', $first_date)->where('orders.created_at', '<=', $last_date)->groupBy('date')->get()->pluck('sum', 'date')->toArray();
 
         for ($i = 1; $i < 32; $i++) {
             if (!isset($days[$i])) {
@@ -80,11 +146,39 @@ class ChartController extends Controller
         return $days;
     }
 
-    public function getRevenueEachCategory()
+    /**
+     *  return \App\Models\Loaisach $categories
+     **/
+    public function getMonthRevenueEachCategory($time = null)
     {
-        $revenue = DB::table('orders')->leftJoin('orderdetails', 'orders.id', '=', 'orderdetails.order_id')->leftJoin('books', 'orderdetails.book_id', '=', 'books.id')->select('loaisach_id as id', DB::raw('sum(sell_price*quantity) as sum'))->where('orders.created_at', '>=', now()->subDays(30)->toDateTimeString())->groupBy('loaisach_id')->get()->pluck('sum', 'id');
+        $date = now();
+
+        if ($time == 'last month') {
+            $first_date = strtotime(date("Y-m-d", strtotime($date)) . ", first day of last month");
+            $first_date = date("Y-m-d", $first_date);
+
+            $last_date = strtotime(date("Y-m-d", strtotime($date)) . ", last day of last month");
+            $last_date = date("Y-m-d", $last_date);
+        } else {
+            $first_date = strtotime(date("Y-m-d", strtotime($date)) . ", first day of this month");
+            $first_date = date("Y-m-d", $first_date);
+
+            $last_date = strtotime(date("Y-m-d", strtotime($date)) . ", last day of this month");
+            $last_date = date("Y-m-d", $last_date);
+        }
+        $revenue = DB::table('orders')->leftJoin('orderdetails', 'orders.id', '=', 'orderdetails.order_id')->leftJoin('books', 'orderdetails.book_id', '=', 'books.id')->select('loaisach_id as id', DB::raw('sum(sell_price*quantity) as sum'))->where('orders.created_at', '>=', $first_date)->where('orders.created_at', '<=', $last_date)->groupBy('loaisach_id')->get()->pluck('sum', 'id');
 
         $revenue = $revenue->toArray();
-        return $revenue;
+
+        $categories = $this->category->all();
+        foreach ($categories as $key => $category) {
+            if (isset($revenue[$category->id])) {
+                $category->revenue = $revenue[$category->id];
+            } else {
+                $categories->forget($key);
+            }
+        }
+
+        return $categories;
     }
 }
--- resources/views/charts/index.blade.php
+++ resources/views/charts/index.blade.php
@@ -4,53 +4,69 @@
 
 @section('content')
     <div style="padding-left: 15px;padding-right: 15px;height: 500px">
-        <h3 style="color:dodgerblue">Month</h3>
+        <h3 style="color:dodgerblue">Current Month</h3>
         <!-- Nav tabs -->
         <ul class="nav nav-tabs" id="myTab" role="tablist" style="background: #3d3939;">
             <li class="nav-item">
-                <a class="nav-link active" id="line-tab" data-toggle="tab" href="#line" role="tab" aria-controls="line" aria-selected="true">Day</a>
+                <a class="nav-link active" id="line-current-month-tab" data-toggle="tab" href="#line-current-month" role="tab" aria-controls="lineCurrentMonth" aria-selected="true">Day</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="bar-tab" data-toggle="tab" href="#bar" role="tab" aria-controls="bar" aria-selected="false">Sum</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="messages-tab" data-toggle="tab" href="#messages" role="tab" aria-controls="messages" aria-selected="false">Messages</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Settings</a>
+                <a class="nav-link" id="doughnut-current-month-tab" data-toggle="tab" href="#doughnut-current-month" role="tab" aria-controls="doughnutCurrentMonth" aria-selected="false">Circle</a>
             </li>
         </ul>
         <br>
 
         <!-- Tab panes -->
         <div class="tab-content">
-            <div class="tab-pane active" id="line" role="tabpanel" aria-labelledby="line-tab">
-                {!! $lineChart->container() !!}
-                {!! $lineChart->script() !!}
+            <div class="tab-pane active" id="line-current-month" role="tabpanel" aria-labelledby="line-tab">
+                {!! $lineCurrentMonthChart->container() !!}
+                {!! $lineCurrentMonthChart->script() !!}
             </div>
-            <div class="tab-pane" id="bar" role="tabpanel" aria-labelledby="bar-tab">
-                <canvas style="height: 0px"></canvas>
-                {!! $barChart->container() !!}
-                {!! $barChart->script() !!}
+            <div class="tab-pane" id="doughnut-current-month" role="tabpanel" aria-labelledby="bar-tab">
+                <canvas style="height: 0px;display: block"></canvas>
+                <div style="height: 400px;">
+                    {!! $doughnutCurrentMonthChart->container() !!}
+                    {!! $doughnutCurrentMonthChart->script() !!}
+                </div>
             </div>
-            <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">mess</div>
-            <div class="tab-pane" id="settings" role="tabpanel" aria-labelledby="settings-tab">setting</div>
         </div>
-
     </div>
 
-    <br><br>
-    <p id="tt">Click this paragraph.</p>
+    <br><br><hr><br><br>
+
+    <div style="padding-left: 15px;padding-right: 15px;height: 500px">
+        <h3 style="color:dodgerblue">Last Month</h3>
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" id="myTab" role="tablist" style="background: #3d3939;">
+            <li class="nav-item">
+                <a class="nav-link active" id="line-last-month-tab" data-toggle="tab" href="#line-last-month" role="tab" aria-controls="line" aria-selected="true">Day</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="doughnut-last-month-tab" data-toggle="tab" href="#doughnut-last-month" role="tab" aria-controls="doughnut" aria-selected="false">Circle</a>
+            </li>
+        </ul>
+        <br>
+
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div class="tab-pane active" id="line-last-month" role="tabpanel" aria-labelledby="line-tab">
+                {!! $lineLastMonthChart->container() !!}
+                {!! $lineLastMonthChart->script() !!}
+            </div>
+            <div class="tab-pane" id="doughnut-last-month" role="tabpanel" aria-labelledby="bar-tab">
+                <canvas style="height: 0px;display: block"></canvas>
+                <div style="height: 400px;">
+                    {!! $doughnutLastMonthChart->container() !!}
+                    {!! $doughnutLastMonthChart->script() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 
     <script>
         sidebar = document.getElementsByClassName('sidebar-wrapper').item(0).getElementsByClassName('nav').item(0);
         sidebar.getElementsByTagName('li').item(0).classList.add('active');
-
-        $(document).ready(function(){
-            $("#tt").on("click", function(){
-                alert("The paragraph was clicked.");
-            });
-        });
     </script>
 @endsection
