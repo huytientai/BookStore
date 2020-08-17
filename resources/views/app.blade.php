70b4324205b7d12198d52f3510191eb328096052
huy nguyen <huytientai1@gmail.com>
2020-08-17 14:25:52 +0700

    before chat test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,5 +3,8 @@
 @section('title','app')
 
 @section('content')
-    <h3>Hello</h3>
+    <div id="app">
+        <h3>Hello</h3>
+        <example-component></example-component>
+    </div>
 @endsection
